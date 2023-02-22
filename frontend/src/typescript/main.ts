718f8197f5495ed03b96f79b3a60663d8f1ef5a3
Kristopher Gray <kristopher.gray99@gmail.com>
2023-02-22 16:10:48 +0100

    clean up embeddedVideoURL code

--- frontend/src/typescript/main.ts
+++ frontend/src/typescript/main.ts
@@ -3,18 +3,14 @@ import { getVideos } from './videos'
 const displayVideo = async () => {
 	const videos = await getVideos()
 
-	if (videos.status !== 'success') {
-		document.querySelector('#')!.classList.remove('hide')
-	}
+	if (videos.status !== 'success') return document.querySelector('#fetch-failed')!.classList.remove('hide')
 
 	const video = videos.data[1]
 
 	document.querySelector('#video-title')!.innerHTML = video.title
 
-	const lastSlash = video.url.lastIndexOf('/')
-	const equalSign = video.url.lastIndexOf('=') + 1
-	const UrlStart = video.url.slice(0, lastSlash + 1)
-	const UrlEnding = video.url.slice(equalSign)
+	const UrlStart = video.url.slice(0, video.url.lastIndexOf('/') + 1)
+	const UrlEnding = video.url.slice(video.url.lastIndexOf('=') + 1)
 	const embeddedVideoURL = `${UrlStart}embed/${UrlEnding}`
 
 	document.querySelector('#video-player')!.innerHTML = `
