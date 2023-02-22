a11cd8998d95241eddabce0b45aac6556ee96ea2
Kristopher Gray <kristopher.gray99@gmail.com>
2023-02-22 15:24:58 +0100

    fetched videos and checks if it was a success.

--- frontend/src/typescript/main.ts
+++ frontend/src/typescript/main.ts
@@ -1,24 +1,33 @@
-import { videos } from './videos'
+import { getVideos } from './videos'
 
-const video = videos[2]
+const displayVideo = async () => {
+	const videos = await getVideos()
 
-document.querySelector('#video-title')!.innerHTML = video.title
+	if (videos.status !== 'success') {
+		document.querySelector('#')!.classList.remove('hide')
+	}
 
-const lastSlash = video.url.lastIndexOf('/')
-const equalSign = video.url.lastIndexOf('=') + 1
-const UrlStart = video.url.slice(0, lastSlash + 1)
-const UrlEnding = video.url.slice(equalSign)
-const embeddedVideoURL = `${UrlStart}embed/${UrlEnding}`
+	const video = videos.data[1]
 
-// const embeddedIframe = `
-// 	<iframe
-// 		width="100%"
-// 		height="100%"
-// 		src=${embeddedVideoURL}
-// 		title="YouTube video player"
-// 		frameborder="0"
-// 		allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-// 		allowfullscreen
-// 	</iframe>
-// `
-// document.querySelector('#video-player')!.innerHTML = embeddedIframe
+	document.querySelector('#video-title')!.innerHTML = video.title
+
+	const lastSlash = video.url.lastIndexOf('/')
+	const equalSign = video.url.lastIndexOf('=') + 1
+	const UrlStart = video.url.slice(0, lastSlash + 1)
+	const UrlEnding = video.url.slice(equalSign)
+	const embeddedVideoURL = `${UrlStart}embed/${UrlEnding}`
+
+	document.querySelector('#video-player')!.innerHTML = `
+		<iframe
+			width="100%"
+			height="100%"
+			src=${embeddedVideoURL}
+			title="YouTube video player"
+			frameborder="0"
+			allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+			allowfullscreen
+		</iframe>
+	`
+}
+
+displayVideo()
--- frontend/src/typescript/videos.ts
+++ frontend/src/typescript/videos.ts
@@ -1,14 +1,4 @@
-export const videos = [
-	{
-		title: `BTS (방탄소년단) '피 땀 눈물 (Blood Sweat & Tears)' Official MV`,
-		url: "https://www.youtube.com/watch?v=hmE9f-TEutc",
-	},
-	{
-		title: `BLACKPINK - ‘Shut Down’ M/V`,
-		url: "https://www.youtube.com/watch?v=POe9SOEKotk",
-	},
-	{
-		title: `TWICE "Talk that Talk" M/V`,
-		url: "https://www.youtube.com/watch?v=k6jqx9kZgPM",
-	},
-]
+export const getVideos = async () => {
+	const res = await fetch('http://localhost:3000/videos')
+	return res.json()
+}
